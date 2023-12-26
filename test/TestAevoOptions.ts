004d7b24b4334c138c34b15c45f263907437342b
Khiem <khiem.bui@transwap.com>
2023-12-26 21:32:02 +0700

    fix fail aevo depositToAppChain test

--- test/TestAevoOptions.ts
+++ test/TestAevoOptions.ts
@@ -7,7 +7,6 @@ const aevoContractAbi = [{"inputs":[{"internalType":"address","name":"token_","t
 
 describe("OptionsTestVault Contract", function () {
   let aevoOptions: Contracts.AevoOptions;
-  let deployer: Signer;
   let user: Signer;
   let bridgedUsdc: Contracts.IERC20;
   const aevoAddress = "0x80d40e32FAD8bE8da5C6A42B8aF1E181984D137c";
@@ -30,97 +29,62 @@ describe("OptionsTestVault Contract", function () {
 
   before(async function () {
     // Get signers
-    [deployer, user] = await ethers.getSigners();
-    console.log("User = %s", await user.getAddress());
-
-    console.log(
-      "Deploying contracts with the account:",
-      await deployer.getAddress()
-    );
+    [user] = await ethers.getSigners();
 
     // Deploy AevoOptions and set optionsVendorProxy
     const aevoOptionsAddress = await deployAevoOptions();
 
     optionsReceiver = await user.getAddress();
 
+    bridgedUsdc = await ethers.getContractAt("IERC20", assetAddress);
+
     const impersonatedSigner = await ethers.getImpersonatedSigner(
       "0x226bf1ee0bb0cf647f6a9f0d8b380d6ab56de3cb"
     );
-    console.log("address 1", await impersonatedSigner.getAddress());
 
-    // Connect to the asset contract (assuming ERC20)
-    bridgedUsdc = await ethers.getContractAt("IERC20", assetAddress);
-    console.log(
-      "balance of 1",
-      await bridgedUsdc
-        .connect(impersonatedSigner)
-        .balanceOf(await impersonatedSigner.getAddress())
-    );
+    console.log("Before Balance eth of impersonatedSigner %s", await ethers.provider.getBalance(impersonatedSigner));
+    const tx = {
+      to: impersonatedSigner,
+      value: ethers.parseEther("100"),
+    };
+    await user.sendTransaction(tx);
+
+    console.log("Balance eth of impersonatedSigner %s", await ethers.provider.getBalance(impersonatedSigner));
 
     const transferTx = await bridgedUsdc
       .connect(impersonatedSigner)
-      .transfer(optionsReceiver, ethers.parseUnits("1000", 6)); // Transferring 1000 USDC
+      .transfer(optionsReceiver, ethers.parseUnits("10000", 6)); // Transferring 10000 USDC
     await transferTx.wait();
-
-    const balanceOfUser = await bridgedUsdc
-      .connect(user)
-      .balanceOf(optionsReceiver);
-    console.log("Balance of user %s", balanceOfUser);
   });
 
-//   it("should deposit tokens to the OptionsTestVault", async function () {
-//     // First, approve the OptionsTestVault contract to spend tokens
-//     const depositAmount = ethers.parseUnits("1000", 6);
-//     const tx1 = await bridgedUsdc
-//       .connect(user)
-//       .approve(aevoAddress, depositAmount);
-//     await tx1.wait();
-
-//     const tx2 = await bridgedUsdc
-//       .connect(user)
-//       .approve(await aevoOptions.getAddress(), depositAmount);
-//     await tx2.wait();
-
-//     // Then, deposit tokens to the OptionsTestVault
-//     await aevoOptions
-//       .connect(user)
-//       .depositToVendor(
-//         await user.getAddress(),
-//         depositAmount,
-//         aevoConnectorAddress
-//       );
-
-//     console.log("deposited to vault");
-
-//   });
   it("should deposit tokens to AEVO directly", async function () {
 
-    // const provider = new ethers.providers.JsonRpcProvider(`https://arbitrum-mainnet.infura.io/v3/85cde589ce754dafa0a57001c326104d`);
-    // First, approve the OptionsTestVault contract to spend tokens
-    const depositAmount = ethers.parseUnits("1000", 6);
-    const tx1 = await bridgedUsdc
-      .connect(user)
-      .approve(aevoAddress, depositAmount);
-    await tx1.wait();
+    const depositAmount = ethers.parseUnits("100", 6);
+
+    const tx2 = await bridgedUsdc
+    .connect(user)
+    .approve(aevoAddress, depositAmount);
+    await tx2.wait();
+
+    console.log("eth Balance eth of user %s", await ethers.provider.getBalance(user));
+    console.log("usdc.e Balance eth of user %s", await bridgedUsdc.connect(user).balanceOf(optionsReceiver));
 
     const aevoContract = new ethers.Contract(aevoAddress, aevoContractAbi, user);
 
-    const tx2 = await bridgedUsdc
-      .connect(user)
-      .approve(aevoAddress, depositAmount);
-    await tx2.wait();
+    optionsReceiver = await user.getAddress();
 
-    // Then, deposit tokens to the OptionsTestVault
-    await aevoContract
-      .connect(user)
-      .depositToAppChain(
-        await user.getAddress(),
-        depositAmount,
-        1000000,
-        aevoConnectorAddress
-      );
+    const tx = await aevoContract.depositToAppChain(
+      optionsReceiver, 
+      100000000, 
+      1000000, 
+      aevoConnectorAddress, 
+      {
+      value: ethers.parseEther('0.1'), // Optional: Sending Ether along with the transaction
+    });
 
+    // Wait for the transaction to be mined
+    await tx.wait();
+    
     console.log("deposited to vault");
-
   });
 });
